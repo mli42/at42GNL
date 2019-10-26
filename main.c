8f7c3e29b1a560980c988272a9e3494059e09341
Marc LI <mli@e3r6p6.42.fr>
2019-10-26 18:15:02 +0200

    main fix ?

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mli <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/26 13:55:01 by mli               #+#    #+#             */
-/*   Updated: 2019/10/26 15:14:12 by mli              ###   ########.fr       */
+/*   Updated: 2019/10/26 18:14:35 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,23 @@ int		main(int argc, char **argv)
 {
 	char	*line;
 	int		fd;
+	int		gnl_r;
 
 	if (argc == 2)
 	{
-		if ((fd = open(argv[1])))
-			while (get_next_line(fd, &line) != -1)
+		if ((fd = open(argv[1], O_RDONLY)))
+		{
+			while (gnl_r = get_next_line(fd, &line) == 1)
 			{
 				printf("%s\n", line);
 				free(line);
 			}
+			if (gnl_r = get_next_line(fd, &line) == 0)
+			{
+				printf("%s\n", line);
+				free(line);
+			}
+		}
 		if (fd)
 			close(fd);
 	}
